e25cfdee789b6d23724531ccc85e0a90b3e6bc5b
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-25 17:16:34 +0700

    Add Layout

---
+++ resources/views/templates/topbar.blade.php
@@ -0,0 +1,40 @@
+        <header>
+            <div class="topbar d-flex align-items-center">
+                <nav class="navbar navbar-expand">
+                    <div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
+                    </div>
+                    <div class="top-menu ms-auto">
+                        <div class="header-notifications-list"></div>
+                        <div class="header-message-list"></div>
+                    </div>
+                    <div class="user-box dropdown">
+                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
+                            <div class="user-info ps-3">
+                                <p class="user-name mb-0">{{ Auth::user()->name }}</p>
+                                <p class="designattion mb-0">Rp. 8.000</p>
+                            </div>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Profile</span></a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="javascript:;"><i class='bx bx-dollar-circle'></i><span>Balance</span></a>
+                            </li>
+
+                            <li>
+                                <a class="dropdown-item" href="javascript:;"><i class='bx bx-download'></i><span>Downloads</span></a>
+                            </li>
+
+                            <li>
+                                <div class="dropdown-divider mb-0"></div>
+                            </li>
+                            <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-log-out-circle'></i><span>Logout</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
