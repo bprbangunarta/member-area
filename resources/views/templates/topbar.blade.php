ab1aa2b820bc1b9ed5152cecc17520eac9a19001
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-25 19:14:06 +0700

    Logout

--- resources/views/templates/topbar.blade.php
+++ resources/views/templates/topbar.blade.php
@@ -8,7 +8,8 @@
                         <div class="header-message-list"></div>
                     </div>
                     <div class="user-box dropdown">
-                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret"
+                            href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
                             <div class="user-info ps-3">
                                 <p class="user-name mb-0">{{ Auth::user()->name }}</p>
@@ -17,21 +18,30 @@
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
-                                <a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Profile</span></a>
+                                <a class="dropdown-item" href="javascript:;"><i
+                                        class="bx bx-user"></i><span>Profile</span></a>
                             </li>
 
                             <li>
-                                <a class="dropdown-item" href="javascript:;"><i class='bx bx-dollar-circle'></i><span>Balance</span></a>
+                                <a class="dropdown-item" href="javascript:;"><i
+                                        class='bx bx-dollar-circle'></i><span>Balance</span></a>
                             </li>
 
                             <li>
-                                <a class="dropdown-item" href="javascript:;"><i class='bx bx-download'></i><span>Downloads</span></a>
+                                <a class="dropdown-item" href="javascript:;"><i
+                                        class='bx bx-download'></i><span>Downloads</span></a>
                             </li>
 
                             <li>
                                 <div class="dropdown-divider mb-0"></div>
                             </li>
-                            <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-log-out-circle'></i><span>Logout</span></a>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <a class="dropdown-item"
+                                        onclick="event.preventDefault(); this.closest('form').submit();"><i
+                                            class='bx bx-log-out-circle'></i><span>Logout</span></a>
+                                </form>
                             </li>
                         </ul>
                     </div>
